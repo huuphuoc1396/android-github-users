bd5d49d6604e006307044fc5e29a185aa70d019b
huuphuoc1396 <huuphuoc1396@gmail.com>
2024-12-28 09:16:21 +0700

    feat: Update README.md

--- domain/src/main/java/com/tyme/github/users/domain/models/errors/ApiException.kt
+++ domain/src/main/java/com/tyme/github/users/domain/models/errors/ApiException.kt
@@ -16,4 +16,4 @@ package com.tyme.github.users.domain.models.errors
 data class ApiException(
     val code: Int = 0,
     override val message: String = "",
-) : RuntimeException()
+) : Exception()
