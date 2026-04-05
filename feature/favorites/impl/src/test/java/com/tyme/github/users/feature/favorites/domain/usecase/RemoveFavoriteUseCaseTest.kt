d5fe54c21289cd3366b8feb20d955018b12b794a
huuphuoc1396 <huuphuoc1396@gmail.com>
2026-04-05 21:04:06 +0700

    test: Add unit tests for use cases and fix UserDetailsViewModelTest

---
+++ feature/favorites/impl/src/test/java/com/tyme/github/users/feature/favorites/domain/usecase/RemoveFavoriteUseCaseTest.kt
@@ -0,0 +1,28 @@
+package com.tyme.github.users.feature.favorites.domain.usecase
+
+import com.tyme.github.users.feature.users.api.repository.FavoriteRepository
+import io.mockk.coJustRun
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+internal class RemoveFavoriteUseCaseTest {
+
+    private val repository = mockk<FavoriteRepository>()
+
+    private val useCase = RemoveFavoriteUseCase(repository)
+
+    @Test
+    fun `invoke calls repository removeFavorite with given username`() = runTest {
+        // Given
+        val username = "user1"
+        coJustRun { repository.removeFavorite(username) }
+
+        // When
+        useCase(username)
+
+        // Then
+        coVerify { repository.removeFavorite(username) }
+    }
+}
---
+++ feature/users/impl/src/test/java/com/tyme/github/users/feature/users/domain/usecase/ObserveFavoriteUseCaseTest.kt
@@ -0,0 +1,56 @@
+package com.tyme.github.users.feature.users.domain.usecase
+
+import app.cash.turbine.test
+import com.tyme.github.users.feature.users.api.repository.FavoriteRepository
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+internal class ObserveFavoriteUseCaseTest {
+
+    private val favoriteRepository = mockk<FavoriteRepository>()
+
+    private val useCase = ObserveFavoriteUseCase(favoriteRepository)
+
+    @Test
+    fun `invoke emits true when username is favorite`() = runTest {
+        // Given
+        val username = "user1"
+        every { favoriteRepository.isFavorite(username) } returns flowOf(true)
+
+        // When / Then
+        useCase(username).test {
+            expectMostRecentItem() shouldBe true
+        }
+    }
+
+    @Test
+    fun `invoke emits false when username is not favorite`() = runTest {
+        // Given
+        val username = "user1"
+        every { favoriteRepository.isFavorite(username) } returns flowOf(false)
+
+        // When / Then
+        useCase(username).test {
+            expectMostRecentItem() shouldBe false
+        }
+    }
+
+    @Test
+    fun `invoke emits multiple values as favorite status changes`() = runTest {
+        // Given
+        val username = "user1"
+        every { favoriteRepository.isFavorite(username) } returns flowOf(false, true, false)
+
+        // When / Then
+        useCase(username).test {
+            awaitItem() shouldBe false
+            awaitItem() shouldBe true
+            awaitItem() shouldBe false
+            awaitComplete()
+        }
+    }
+}
